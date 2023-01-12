6ffbc629b5e995d6fc4da33395ba5472b7e307dd
Kromus22 <kromus2@gmail.com>
2023-01-12 16:59:01 +0500

    +адаптив

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
+//menu
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', () => {
+  menuMobile.classList.toggle('menu--open');
+});
+
+
 //slider
 const swiper = new Swiper('.swiper', {
   loop: true,
-  slidesPerView: 2,
-  spaceBetween: 20,
 
   navigation: {
     nextEl: '.swiper-button-right',
     prevEl: '.swiper-button-left',
   },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    }
+  }
 });
 
 //map
@@ -16,7 +33,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -37.23198599342622, lng: -8.628183046816766 },
+    center: { lat: 37.23198599342622, lng: -8.628183046816766 },
     zoom: 13,
     styles:
       [
